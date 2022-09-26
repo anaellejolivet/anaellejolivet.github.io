3ed072015a3cd900aea40309645b42dab9a3ddf1
Anaelle Jolivet <anaelle0jolivet@gmail.com>
2022-09-26 10:55:14 -0400

    Lab03

--- Labs/Lab03Points.js
+++ Labs/Lab03Points.js
@@ -11,9 +11,12 @@ function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
     
     points=[
-      vec2(   ,   ),
-      vec2(   ,   ),
-      vec2(   ,   )
+      vec2(0,0),
+      vec2(-1,0),
+      vec2(0,1),
+      vec2(1,1),
+      vec2(-1,-1),
+      vec2(0.2,-0,7)
     ];
     
     //
